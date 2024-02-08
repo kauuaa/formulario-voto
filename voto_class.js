e718b19ac7d35fca3b15a80039fc13b8ea8c268e
Kauã Biasebetti <kauamengel17@gmail.com>
2024-02-08 11:48:27 -0300

    Formulário pronto

--- voto_class.js
+++ voto_class.js
@@ -1,7 +1,22 @@
-class eleitor{
+class Eleitor{
     constructor(nome, idade, titulo){
-        this.name = nome
+        this.nome = nome
         this.idade = idade
         this.titulo = titulo
+    }
+
+    verificaVoto(){
+        let idade = this.idade
+
+        if(idade < 16){
+            return `o voto não é permitido pois não atinge a idade mínima!`   
+        }else if(idade >= 16 && idade < 18){
+            return `o voto é facultativo!`
+        }else if(idade >= 18 && idade <= 70){
+            return `o voto é obrigatório!`
+        }else if(idade >= 100){
+            window.alert ("Insira uma idade válida!")
+            return 
+        }
     }
 }
--- voto_main.js
+++ voto_main.js
@@ -7,11 +7,19 @@ verificar.addEventListener('click', ()=>{
     let titulo = Number(document.getElementById('titulo').value)
 
     console.log("Dados do formulário = " + nome, idade, titulo)
-})
 
-const cidadao = new eleitor()
+
+const cidadao = new Eleitor() 
+
     cidadao.nome = nome
     cidadao.idade = idade
     cidadao.titulo = titulo
 
-    console.log('Instância do objeto', cidadao)
+    console.log(`Instância do objeto, ${cidadao}`)
+
+    let mensagem = cidadao.verificaVoto()
+    
+    resposta.innerHTML = ''
+    resposta.innerHTML = `Para o eleitor denominado ${cidadao.nome}, com o título de número "${cidadao.titulo}", e com ${cidadao.idade} anos de idade, ${mensagem}`
+    
+})
